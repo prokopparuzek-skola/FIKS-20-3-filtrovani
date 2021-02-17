c90eccbae11c41f543e5c1616f5bd06e6437e882
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2021-02-17 22:50:42 +0100

    go ready na odevzdani

--- filtrovani.go
+++ filtrovani.go
@@ -14,34 +14,39 @@ func main() {
 
 	fmt.Scan(&M)
 	for v := 0; v < M; v++ {
-		fmt.Println(v)
-		fmt.Scan(&N, &P)
-		words = make([]string, N)
-		for i := 0; i < 10; i++ {
-			var count int
-			fmt.Scan(&count)
-			fmt.Scan(&tmp)
-			for j := 0; j < count; j++ {
-				table[tmp[j]] = i
+		if v < 10 {
+			fmt.Scan(&N, &P)
+			words = make([]string, N)
+			for i := 0; i < 10; i++ {
+				var count int
+				fmt.Scan(&count)
+				fmt.Scan(&tmp)
+				for j := 0; j < count; j++ {
+					table[tmp[j]] = i
+				}
+			}
+			for i := 0; i < N; i++ {
+				var count int
+				fmt.Scan(&count)
+				fmt.Scan(&tmp)
+				for j := 0; j < count; j++ {
+					words[i] += fmt.Sprint(table[tmp[j]])
+				}
+				words[i] = "(" + words[i] + ")"
+			}
+			reg := regexp.MustCompile(strings.Join(words, "|"))
+			for i := 0; i < P; i++ {
+				var hum string
+				var count int
+				fmt.Scan(&count)
+				fmt.Scanln(&hum)
+				indexes := reg.FindIndex([]byte(hum))
+				if indexes == nil {
+					fmt.Println(-1)
+				} else {
+					fmt.Println(indexes[0])
+				}
 			}
 		}
-		for i := 0; i < N; i++ {
-			var count int
-			fmt.Scan(&count)
-			fmt.Scan(&tmp)
-			for j := 0; j < count; j++ {
-				words[i] += fmt.Sprint(table[tmp[j]])
-			}
-			words[i] = "(" + words[i] + ")"
-		}
-		reg := regexp.MustCompile(strings.Join(words, "|"))
-		for i := 0; i < P; i++ {
-			var hum string
-			var count int
-			fmt.Scan(&count)
-			fmt.Scanln(&hum)
-			fmt.Println(reg.FindIndex([]byte(hum)))
-		}
-
 	}
 }

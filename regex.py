e51255f78a2153d7a02291071402297d8e81c279
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2021-02-18 13:43:47 +0100

    python slow
    spravny vystup, pro kontrolu

--- regex.py
+++ regex.py
@@ -4,21 +4,25 @@ import re
 
 M = int(input())
 for v in range(M):
-    print(v)
-    N, P = (int(n) for n in input().split())
-    table = {}
-    words = []
-    for i in range(10):
-        codes = list(input().split()[1])
-        for c in codes:
-            table[c] = str(i)
-    for _ in range(N):
-        decode = ""
-        word = list(input().split()[1])
-        for c in word:
-            decode += table[c]
-        words.append('('+decode+')')
-    reg = re.compile('|'.join(words))
-    for _ in range(P):
-        hum = input().split()[1]
-        print(reg.search(hum))
+    if v < 10:
+        N, P = (int(n) for n in input().split())
+        table = {}
+        words = []
+        for i in range(10):
+            codes = list(input().split()[1])
+            for c in codes:
+                table[c] = str(i)
+        for _ in range(N):
+            decode = ""
+            word = list(input().split()[1])
+            for c in word:
+                decode += table[c]
+            words.append('('+decode+')')
+        reg = re.compile('|'.join(words))
+        for _ in range(P):
+            hum = input().split()[1]
+            index = reg.search(hum)
+            if index is None:
+                print(-1)
+            else:
+                print(index.start(0))
